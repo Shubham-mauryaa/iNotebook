80158857b93d342cb12bf29ad1fded1c05a1e18b
Shubham-mauryaa <shubham58268@gmail.com>
2024-06-01 14:01:00 +0530

    second commit

--- index.js
+++ index.js
@@ -4,11 +4,7 @@ const express = require('express')
 connectToMongo();
 
 const app = express()
-const port = 3000
-
-// app.get('/', (req, res) => {
-//   res.send('Hello Shubham!.......this is icloud')
-// })
+const port = 5000
 
  //it is a middleware that is required to request body content in any mongoose models for example in auth.js it will show undefined for console.log(req.body) even after giving some content to the body because of not using middleware 
  app.use(express.json())
--- routes/auth.js
+++ routes/auth.js
@@ -3,27 +3,42 @@ const User = require('../models/Users');
 const { body, validationResult } = require('express-validator');
 const router = express.Router();
 
-
-router.post('/', [
+// Create a user using :post "api/auth/createuser". no login required
+router.post('/createuser', [
     body('name').isLength({ min: 3 }),
     body('email').isEmail(),
-    body('username').isLength({ min:5 }),
+    body('username').isLength({ min: 5 }),
     body('password').isLength({ min: 7 })
-], (req, res) => {
-   
+], async (req, res) => {
+
+    // if errors are present show the error message and return bad request
     const result = validationResult(req);
     if (!result.isEmpty()) {
-        return res.status(400).json({errors: result.array()});
+        return res.status(400).json({ errors: result.array() });
     }
-    User.create({
-        name : req.body.name,
-        password : req.body.password,
-        username : req.body.username,
-        email : req.body.email
-    }).then(User=>res.json(User))
-    .catch(err=>{console.log(err) 
-        res.json({error:"please check if email and username both have unque value", message:err.message })}) 
- 
-}) 
 
-module.exports = router;       
+    //check whether user with same email and username existx
+    try {
+
+
+        let user = await User.findOne({ email: req.body.email, username: req.body.username })
+        if (user) {
+            return res.status(400).json({ error: "Sorry a user exist either with same email or username or both" })
+        }
+
+        user = await User.create({
+            name: req.body.name,
+            password: req.body.password,
+            username: req.body.username,
+            email: req.body.email
+        })
+
+        res.json({ user })
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).send("error occured")
+    }
+
+})
+
+module.exports = router;        
