6402b482623bb32a130356f4f9333c165c86235a
Shubham-mauryaa <shubham58268@gmail.com>
2024-06-02 23:19:45 +0530

    auth token added

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const User = require('../models/Users');
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const router = express.Router();
+
+const JWT_SECRET = "I am $hubham"
 
 // Create a user using :post "api/auth/createuser". no login required
 router.post('/createuser', [
     body('name').isLength({ min: 3 }),
     body('email').isEmail(),
-    body('username').isLength({ min: 5 }),
+    body('username').isLength({ min: 3 }),
     body('password').isLength({ min: 7 })
 ], async (req, res) => {
 
@@ -17,28 +21,39 @@ router.post('/createuser', [
         return res.status(400).json({ errors: result.array() });
     }
 
-    //check whether user with same email and username existx
+    //check whether user with same email and username exist
     try {
-
-
         let user = await User.findOne({ email: req.body.email, username: req.body.username })
         if (user) {
             return res.status(400).json({ error: "Sorry a user exist either with same email or username or both" })
         }
 
+        const salt = await bcrypt.genSalt(10);
+        const secpass = await bcrypt.hash(req.body.password,salt) ; //secure password
+
+        //creating new user
         user = await User.create({
             name: req.body.name,
-            password: req.body.password,
+            password: secpass,
             username: req.body.username,
             email: req.body.email
         })
 
-        res.json({ user })
+        const data = {
+            user :{
+                id:user.id
+            }
+        }
+
+        const authtoken = jwt.sign(data, JWT_SECRET)
+        
+        res.json({authtoken})
+        // res.json({ user })
     } catch (error) {
         console.log(error.message)
         res.status(500).send("error occured")
     }
-
+ 
 })
 
 module.exports = router;        
